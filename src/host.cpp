d6be606528bd261b0f8fe617596618a21814157b
chengxing <zcx5613@gmail.com>
2024-07-08 01:18:59 +0900

    update

--- src/host.cpp
+++ src/host.cpp
@@ -1,3 +1,11 @@
 #include "host.hpp"
 
-void matrixAddHost();
+#include <iostream>
+
+void matrixAddHost(){
+#pragma omp parallel for
+    for (int i = 0; i < 100; ++i) {
+        std::cout << i << std::endl;
+    }
+}
+
--- src/main.cu
+++ src/main.cu
@@ -1,32 +1,21 @@
 #include <iostream>
 #include "host.hpp"
 #include "kernel.cuh"
+#include "helper_cuda.h"
 
 #define MATRIX_M 1024
 #define MATRIX_N 1024
 #define MATRIX_K 1024
-
-#define cudaErrCheck(stat) { cudaErrCheck_((stat), __FILE__, __LINE__); }
-void cudaErrCheck_(cudaError_t stat, const char *file, int line) {
-    if (stat != cudaSuccess) {
-        fprintf(stderr, "CUDA Error: %s %s %d\n", cudaGetErrorString(stat), file, line);
-    }
-}
 
 int main() {
     float *A;
     float *B;
     float *C;
 
-    cudaErrCheck(cudaMalloc((void **) &a_fp32, MATRIX_M * MATRIX_K * sizeof(float)));
-    A = (float *) malloc(MATRIX_M * MATRIX_K * sizeof(float));
-    B = (float *) malloc(MATRIX_K * MATRIX_N * sizeof(float));
-    B = (float *) malloc(MATRIX_K * MATRIX_N * sizeof(float));
+    checkCudaErrors(cudaMalloc((void **) &A, MATRIX_M * MATRIX_K * sizeof(float)));
+    checkCudaErrors(cudaMalloc((void **) &B, MATRIX_K * MATRIX_N * sizeof(float)));
+    checkCudaErrors(cudaMalloc((void **) &C, MATRIX_M * MATRIX_N * sizeof(float)));
 
-#pragma omp parallel for
-    for (int i = 0; i < 100; ++i) {
-        std::cout << i << std::endl;
-    }
 
     return 0;
 }
