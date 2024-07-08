0e1ff613ecfd71b5ad966ada0b81e1106b5ad33f
chengxing <zcx5613@gmail.com>
2024-07-09 02:40:54 +0900

    update

--- src/main.cu
+++ src/main.cu
@@ -8,13 +8,17 @@
 #define MATRIX_K 1024
 
 int main() {
-    float *A;
-    float *B;
-    float *C;
+    float aHost;
+    float bHost;
+    float cHost;
 
-    checkCudaErrors(cudaMalloc((void **) &A, MATRIX_M * MATRIX_K * sizeof(float)));
-    checkCudaErrors(cudaMalloc((void **) &B, MATRIX_K * MATRIX_N * sizeof(float)));
-    checkCudaErrors(cudaMalloc((void **) &C, MATRIX_M * MATRIX_N * sizeof(float)));
+    float *aDev;
+    float *bDev;
+    float *cDev;
+
+    checkCudaErrors(cudaMalloc((void **) &aDev, MATRIX_M * MATRIX_K * sizeof(float)));
+    checkCudaErrors(cudaMalloc((void **) &bDev, MATRIX_K * MATRIX_N * sizeof(float)));
+    checkCudaErrors(cudaMalloc((void **) &cDev, MATRIX_M * MATRIX_N * sizeof(float)));
 
 
     return 0;
