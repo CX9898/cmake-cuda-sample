794d3e5ab110b67e55a0c358131e6fc376110794
ZOU CHENGXING <zcx5613@gmail.com>
2024-08-19 15:12:42 +0900

    Update CudaTimeCalculator.hpp

--- include/CudaTimeCalculator.hpp
+++ include/CudaTimeCalculator.hpp
@@ -1,20 +1,56 @@
 #pragma once
 
+#include <cstdio>
 #include <cuda_runtime.h>
+
+namespace cudaTimeCalculator {
+void cudaErrCheck_(cudaError_t stat) {
+    if (stat != cudaSuccess) {
+        fprintf(stderr, "CudaTimeCalculator Error: %s \n", cudaGetErrorString(stat));
+    }
+}
+} // namespace cudaTimeCalculator
 
 class CudaTimeCalculator {
  public:
-  CudaTimeCalculator();
-  ~CudaTimeCalculator();
+  CudaTimeCalculator() {
+      time_ = 0.0f;
 
-  void startClock();
-  void endClock();
-  float getTime();
+      cudaTimeCalculator::cudaErrCheck_(cudaEventCreate(&star_));
+      cudaTimeCalculator::cudaErrCheck_(cudaEventCreate(&stop_));
+  }
+
+  ~CudaTimeCalculator() {
+      cudaTimeCalculator::cudaErrCheck_(cudaEventDestroy(star_));
+      cudaTimeCalculator::cudaErrCheck_(cudaEventDestroy(stop_));
+  }
+
+  void reset(){
+      cudaTimeCalculator::cudaErrCheck_(cudaEventDestroy(star_));
+      cudaTimeCalculator::cudaErrCheck_(cudaEventDestroy(stop_));
+
+      cudaTimeCalculator::cudaErrCheck_(cudaEventCreate(&star_));
+      cudaTimeCalculator::cudaErrCheck_(cudaEventCreate(&stop_));
+  }
+
+  void startClock() {
+      cudaTimeCalculator::cudaErrCheck_(cudaEventRecord(star_));
+  }
+
+  void endClock() {
+      cudaTimeCalculator::cudaErrCheck_(cudaEventRecord(stop_));
+      cudaTimeCalculator::cudaErrCheck_(cudaEventSynchronize(stop_));
+  }
+
+  float getTime() {
+      cudaTimeCalculator::cudaErrCheck_(cudaEventElapsedTime(&time_, star_, stop_));
+      return time_;
+  }
 
  private:
-  cudaEvent_t _star;
-  cudaEvent_t _stop;
+  cudaEvent_t star_;
+  cudaEvent_t stop_;
 
-  float _time;
+  float time_;
 };
 
